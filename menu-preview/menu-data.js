8642ba62e56bb77b02753cab5c13a588231831a8
Hang Nguyen <109588620+hang-2707@users.noreply.github.com>
2026-09-17 08:34:30 +0700

    Thêm bản xem trước menu: menu-data.js

---
+++ menu-preview/menu-data.js
@@ -0,0 +1,22 @@
+// Chỉnh tên món, giá và tên ảnh tại đây.
+// Ảnh đặt trong thư mục images, nên dùng ảnh ngang tỉ lệ 4:3.
+window.MENU_ITEMS = [
+  {
+    id: "mon-01",
+    name: "Món đặc biệt 01",
+    price: 35000,
+    image: "images/mon-01.jpg"
+  },
+  {
+    id: "mon-02",
+    name: "Món đặc biệt 02",
+    price: 45000,
+    image: "images/mon-02.jpg"
+  },
+  {
+    id: "mon-03",
+    name: "Món đặc biệt 03",
+    price: 55000,
+    image: "images/mon-03.jpg"
+  }
+];
