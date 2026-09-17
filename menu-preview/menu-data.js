1103d44bd303ae2806cc57ed133caeca57b645f2
Hang Nguyen <109588620+hang-2707@users.noreply.github.com>
2026-09-17 08:54:49 +0700

    Làm mới giao diện, tiền tệ và thông tin cuối trang: menu-data.js

--- menu-preview/menu-data.js
+++ menu-preview/menu-data.js
@@ -5,21 +5,21 @@ window.MENU_ITEMS = [
     id: "mon-01",
     nameFr: "Plat spécial 01",
     nameEn: "Special dish 01",
-    price: 35000,
+    price: 12.5,
     images: ["images/mon-01-1.jpg", "images/mon-01-2.jpg", "images/mon-01-3.jpg"]
   },
   {
     id: "mon-02",
     nameFr: "Plat spécial 02",
     nameEn: "Special dish 02",
-    price: 45000,
+    price: 14,
     images: ["images/mon-02-1.jpg", "images/mon-02-2.jpg", "images/mon-02-3.jpg"]
   },
   {
     id: "mon-03",
     nameFr: "Plat spécial 03",
     nameEn: "Special dish 03",
-    price: 55000,
+    price: 16.5,
     images: ["images/mon-03-1.jpg", "images/mon-03-2.jpg", "images/mon-03-3.jpg"]
   }
 ];
