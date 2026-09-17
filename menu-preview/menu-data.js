1efcd22f1bc52ed3d935ec074f206ebaa0da15d1
Hang Nguyen <109588620+hang-2707@users.noreply.github.com>
2026-09-17 08:41:18 +0700

    Cập nhật menu song ngữ và danh sách gọi món: menu-data.js

--- menu-preview/menu-data.js
+++ menu-preview/menu-data.js
@@ -3,19 +3,22 @@
 window.MENU_ITEMS = [
   {
     id: "mon-01",
-    name: "Món đặc biệt 01",
+    nameFr: "Plat spécial 01",
+    nameEn: "Special dish 01",
     price: 35000,
     image: "images/mon-01.jpg"
   },
   {
     id: "mon-02",
-    name: "Món đặc biệt 02",
+    nameFr: "Plat spécial 02",
+    nameEn: "Special dish 02",
     price: 45000,
     image: "images/mon-02.jpg"
   },
   {
     id: "mon-03",
-    name: "Món đặc biệt 03",
+    nameFr: "Plat spécial 03",
+    nameEn: "Special dish 03",
     price: 55000,
     image: "images/mon-03.jpg"
   }
